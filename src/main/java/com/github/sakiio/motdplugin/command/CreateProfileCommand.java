1e874dfe9f13fc0c581d291d76cee9941aa4f1c7
Sakio <65667910+Sakiio@users.noreply.github.com>
2022-08-18 13:03:22 +0200

    Si claro que si

--- src/main/java/com/github/sakiio/motdplugin/command/CreateProfileCommand.java
+++ src/main/java/com/github/sakiio/motdplugin/command/CreateProfileCommand.java
@@ -1,10 +1,13 @@
 package com.github.sakiio.motdplugin.command;
 
+import com.github.sakiio.motdplugin.manager.ProfileManager;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
 public class CreateProfileCommand extends Command {
+
     public CreateProfileCommand() {
+
         super("createmotd", "bungee.create");
     }
 
---
+++ src/main/java/com/github/sakiio/motdplugin/manager/ProfileManager.java
@@ -0,0 +1,24 @@
+package com.github.sakiio.motdplugin.manager;
+
+public class ProfileManager {
+
+    public void createMotd(String name, String motdMessage) {
+
+    }
+
+    public void deleteMotd (String name) {
+
+    }
+
+    public void updateMotd (String name){
+
+    }
+
+    public void renameMotdName(String name, String newName) {
+
+    }
+
+    public void load() {
+
+    }
+}
